fbf495017f8262ee4b0073b0f07f0d6549935a88
Ishank <isainger29@gmail.com>
2022-10-11 14:28:21 +0530

    commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Ishank code\Python 2022\Coding\Django\carzone-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
